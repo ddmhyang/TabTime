e6fd0058b5c8053ca8f3da0f1c04e26dd52d5059
가현 김 <kimgh0127@gmail.com>
2025-11-22 01:57:48 +0900

    초기 커밋.

---
+++ TabTime/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TabTime
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
